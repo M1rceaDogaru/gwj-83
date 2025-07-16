758490034431d5b6b5eb1df7bf03503e006161c4
Kenny Huang <kenny.huang@red-gate.com>
2025-07-16 18:08:16 +0100

    Add mob spawning

---
+++ src/map/map.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+
+func _ready():
+	$MobTimer.start()
+
+func _on_mob_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	# Set the mob's position to the random location.
+	mob.position = mob_spawn_location.position
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
