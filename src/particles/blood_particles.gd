3999a788b7dac7554fc53a11fc5da73d29d43e05
Kenny Huang <kenny.huang@red-gate.com>
2025-07-18 22:03:21 +0100

    particle fade

--- src/particles/blood_particles.gd
+++ src/particles/blood_particles.gd
@@ -5,5 +5,9 @@ extends GPUParticles2D
 func _ready() -> void:
 	$Timer.start(duration)
 
+func _process(delta: float) -> void:
+	var color = process_material.color
+	process_material.color = Color(color.r, color.g, color.b, $Timer.time_left/duration)
+
 func _on_timer_timeout() -> void:
 	queue_free()
